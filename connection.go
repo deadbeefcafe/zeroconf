f504105410f474e356bc044fd0da2df946028d23
David Sharp <dsharp@actiontarget.com>
2017-07-21 22:42:30 -0700

    make less chatty

--- connection.go
+++ connection.go
@@ -49,12 +49,12 @@ func joinUdp6Multicast(interfaces []net.Interface) (*ipv6.PacketConn, error) {
 	if len(interfaces) == 0 {
 		interfaces = listMulticastInterfaces()
 	}
-	log.Println("Using multicast interfaces: ", interfaces)
+	// DJS log.Println("Using multicast interfaces: ", interfaces)
 
 	var failedJoins int
 	for _, iface := range interfaces {
 		if err := pkConn.JoinGroup(&iface, &net.UDPAddr{IP: mdnsGroupIPv6}); err != nil {
-			log.Println("Udp6 JoinGroup failed for iface ", iface)
+			// DJS log.Println("Udp6 JoinGroup failed for iface ", iface)
 			failedJoins++
 		}
 	}
@@ -80,12 +80,12 @@ func joinUdp4Multicast(interfaces []net.Interface) (*ipv4.PacketConn, error) {
 	if len(interfaces) == 0 {
 		interfaces = listMulticastInterfaces()
 	}
-	log.Println("Using multicast interfaces: ", interfaces)
+	// DJS log.Println("Using multicast interfaces: ", interfaces)
 
 	var failedJoins int
 	for _, iface := range interfaces {
 		if err := pkConn.JoinGroup(&iface, &net.UDPAddr{IP: mdnsGroupIPv4}); err != nil {
-			log.Println("Udp4 JoinGroup failed for iface ", iface)
+			// DJS log.Println("Udp4 JoinGroup failed for iface ", iface)
 			failedJoins++
 		}
 	}
